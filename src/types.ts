f18436cded4047993266d75292f6b67a6ebecb8a
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2026-03-11 14:45:04 -0700

    feat: add utility types for Post (Draft, Update, Readonly)

--- src/types.ts
+++ src/types.ts
@@ -5,4 +5,7 @@ export interface Post {
     userId: number;
 }
 
-export type PostPreview = Pick<Post, "id" | "title">
+export type PostPreview = Pick<Post, "id" | "title"> // Pick - Only what you need
+export type PostDraft = Omit<Post, "id" | "userId"> // Omit - Everything but
+export type PostUpdate = { id: number } & Partial<Omit<Post, "id">> // Partial - everything becomes optional
+export type ReadonlyPost = Readonly<Post> // Readonly - everything becomes immutable
